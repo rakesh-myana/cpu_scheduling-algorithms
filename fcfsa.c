88a0f837c585468185c5351a4f10facc14316ffd
rakesh-myana <myanarakesh55@gmail.com>
2024-06-24 22:11:52 +0530

    Update fcfsa.c

--- fcfsa.c
+++ fcfsa.c
@@ -1,12 +1,13 @@
+//FCFS with arrival_time
 #include<stdio.h>
 void main()
 {
 	printf("Enter total number of process:");
 	int n,i,j;
 	scanf("%d",&n);
-	int p[n],at[n],bt[n],ct[n],tat[n],wt[n];
-	float ttat=0,twt=0;
-	for(i=0 ; i<n ; i++)
+	int p[n],at[n],bt[n],ct[n],tat[n],wt[n];	//at=arrival , bt=burst	,ct=completion , tat=turn_around ,wt=waiting times
+	float ttat=0,twt=0;		//ttat=total_turn_around_time , twt=total_waiting_time
+	for(i=0 ; i<n ; i++) 	//Scanning Arrival and Burst times
 	{
 		printf("Enter Process-%d Arrival Time:",i+1);
 		scanf("%d",&at[i]);
@@ -14,7 +15,7 @@ void main()
 		scanf("%d",&bt[i]);
 		p[i]=i+1;
 	}
-	for(i=0; i<(n-1); i++)
+	for(i=0; i<(n-1); i++)	//Arrange in ascending order based on arrival times
     {
         for( j=0; j<(n-1-i); j++)
         {
@@ -34,19 +35,19 @@ void main()
             }
         }
     }
-    wt[0]=0,ct[0]=bt[0]+at[0];
+    wt[0]=0,ct[0]=bt[0]+at[0]; //calculating waiting ,Completion time for process-1 after sorting
 	for(i=1 ; i<n ; i++)
 	{
 		ct[i]=ct[i-1]+bt[i];	
 	}
-	for(i=0 ; i<n ; i++)
+	for(i=0 ; i<n ; i++)	//calculating remaining times
 	{
-		tat[i]=ct[i]-at[i];
+		tat[i]=ct[i]-at[i];	
 		wt[i]=tat[i]-bt[i];
 		twt=twt+wt[i];
 		ttat=ttat+tat[i];	
 	}
-	printf("Process  Arrival_Time  Burst_Time  Compilation_Time   TurnAround_Time   Wating_time\n");
+	printf("Process  Arrival_Time  Burst_Time  Completion_Time   TurnAround_Time   Wating_time\n");
 	for(i=0 ; i<n ; i++)
 	{
 		printf("  P%d\t\t%d\t   %d\t\t%d\t\t\t%d\t\t%d\n",p[i],at[i],bt[i],ct[i],tat[i],wt[i]);
